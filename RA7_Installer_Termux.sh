d00a0eb3a232974df8c544fa9990b5db6266f6bd
iD01t Softwares <itechinfomtl@gmail.com>
2025-07-03 09:21:16 -0400

    Add files via upload

---
+++ RA7_Installer_Termux.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Activate virtual environment
+source $HOME/ra7_env/bin/activate
+
+# Change to working directory
+cd $HOME/RA7
+
+# Start assistant
+python3 conscious_desktop_assistant.py
